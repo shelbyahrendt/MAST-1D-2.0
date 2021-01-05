4594753fa6f621b6c0711136c39543134c32dac7
shelbyahrendt <shelby.ahrendt@gmail.com>
2021-01-05 13:18:48 -0600

    created environment file and tested package installation required for quick-start manual

---
+++ test_pkg.py
@@ -0,0 +1,37 @@
+'''
+Testing all packages are installed as required by the
+MAST-1D Quick-Start document by using conda and the partial package list in the
+environment.yml file copied here for convenience:
+----------------
+name: MAST1Dpy27
+channels:
+  - defaults
+dependencies:
+  - python=2.7
+  - matplotlib
+  - numpy
+  - pandas
+  - xlrd
+-----------------
+
+Note: creating the environment using the full package list from the
+MAST-1D Quick-Start did not work since many of the packages come
+with the python 2.7 installation
+
+'''
+
+import csv
+import datetime
+import copy #this includes 'deepcopy'
+import itertools
+import json
+import math
+import matplotlib
+import numpy
+import os
+import pandas
+import pickle
+import sys
+import tkFileDialog
+import Tkinter
+import xlrd
